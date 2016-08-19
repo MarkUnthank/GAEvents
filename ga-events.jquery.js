9d8ded01554e0fcbc98ae8d573f63edd6469226a
Mark Unthank <markunthank@me.com>
2016-08-19 12:57:06 +0100

    Update ga-events.jquery.js

--- ga-events.jquery.js
+++ ga-events.jquery.js
@@ -5,52 +5,51 @@
  * @license MIT
 **/
 (function( $ ){
-    // Listeners
-    var _track = [ $( ".ga-track" ) ];
+  // Listeners
+  var _track = [ $( ".ga-track" ) ];
 
-    _track.forEach( setup );
+  _track.forEach( setup );
 
-    function setup( element, index, array ) {
-        var _ = new event( element, index, array );
-        _.trackEvent();
-    }
+  function setup( element, index, array ) {
+    var _ = new event( element, index, array );
+    _.trackEvent();
+  }
 
-    function event( element, index, array ) {
-        e = element, i = index, a = array;
-        this.trackEvent = function() {
-            if ( e.length ) {
-                e.click( function( event ){
-                    event.preventDefault();
-                    var _ = $( this ),
-                        h = _.attr( "href" ),
-                        t = _.attr( "target" ),
-                        c = _.attr( "data-ec" ),
-                        a = _.attr( "data-ea" ),
-                        l = _.attr( "data-el" ),
-                        v = _.attr( "data-ev" );
-                    sendEvent( c , a , l , v );
+  function event( element, index, array ) {
+    e = element, i = index, a = array;
+    this.trackEvent = function() {
+      if ( e.length ) {
+        e.click( function( event ){
+          event.preventDefault();
+          var _ = $( this ),
+          h = _.attr( "href" ),
+          t = _.attr( "target" ),
+          c = _.attr( "data-ec" ),
+          a = _.attr( "data-ea" ),
+          l = _.attr( "data-el" ),
+          v = _.attr( "data-ev" );
+          sendEvent( c , a , l , v );
 
-                    if ( t === "_blank" ) {
-                        window.open( h , t );
-                    } else if ( h !== undefined ) {
-                        window.location = h;
-                    } else {
-                        _.unbind('click').click();
-                    }
-                    
-                });
-            }
-        };
-        sendEvent = function( c , a , l , v ){
-            if ( v === undefined ) { v = 1; }
-            ga('send', {
-              'hitType': 'event',
-              'eventCategory': c,
-              'eventAction': a,  
-              'eventLabel': l,
-              'eventValue': v
-            });
-        };
-    }
-   
-})( jQuery );
+          if ( t === "_blank" ) {
+            window.open( h , t );
+          } else if ( h !== undefined ) {
+            window.location = h;
+          } else {
+            _.unbind('click').click();
+          }
+
+        });
+      }
+    };
+    sendEvent = function( c , a , l , v ){
+      if ( v === undefined ) { v = 1; }
+      ga('send', {
+        'hitType': 'event',
+        'eventCategory': c,
+        'eventAction': a,  
+        'eventLabel': l,
+        'eventValue': v
+      });
+    };
+  }
+  })( jQuery );
